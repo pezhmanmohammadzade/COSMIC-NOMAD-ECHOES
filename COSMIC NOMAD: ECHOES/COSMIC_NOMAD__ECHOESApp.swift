5b354bf57f218947ef4c81aa18fd93785d8475ae
pezhmanmohammadzade <pmohammadzadehkordestani25@fed.idserve.net>
2026-05-21 16:31:19 +0200

    Initial Commit

---
+++ COSMIC NOMAD: ECHOES/COSMIC_NOMAD__ECHOESApp.swift
@@ -0,0 +1,17 @@
+//
+//  COSMIC_NOMAD__ECHOESApp.swift
+//  COSMIC NOMAD: ECHOES
+//
+//  Created by pezhman mohammadzadehkordestani on 21/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct COSMIC_NOMAD__ECHOESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
